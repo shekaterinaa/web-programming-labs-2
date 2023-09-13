ac38f637749f064500a1c5002949ef56152e580c
Шкуропатова Екатерина <katygetty98@mail.ru>
2023-09-13 17:19:02 +0700

    Меню

--- app.py
+++ app.py
@@ -8,19 +8,38 @@ def start():
 <!doctype html>
 <html>
     <head>
-        <title> Шкуропатова Екатерина Александровна, лабораторная 1</title>
+        <title> НГТУ, ФБ, Лабораторные работы</title>
     </head>
     <body>
         <header>
-            НГТУ, ФБ, Лабораторная работа 1
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
         </header>
-
-        <h1>web-сервер на flask</h1>
-
+        <h1><a href="/lab1">Меню </a></h1>
         <footer>
             &copy; Шкуропатова Екатерина, ФБИ-13 , 3 курс, 2023
         </footer>
     </body>
 </html>
 """
-    
+
+@app.route("/lab1")
+def lab1():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title> Шкуропатова Екатерина Александровна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+
+           НГТУ, ФБ, Лабораторная работа 1
+
+        </header>
+        <h1>web-сервер на flask</h1>
+        <footer>
+            &copy; Шкуропатова Екатерина, ФБИ-13 , 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
