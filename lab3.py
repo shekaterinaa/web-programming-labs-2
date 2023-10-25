a8d37a21989691e8de9a5715f3615dccdd265926
Шкуропатова Екатерина <katygetty98@mail.ru>
2023-10-25 17:40:51 +0700

    Валидация

--- lab3.py
+++ lab3.py
@@ -1,19 +1,20 @@
-from flask import Blueprint, Flask, render_template, request
+from flask import Blueprint,render_template,request
 lab3 = Blueprint('lab3',__name__)
 
 @lab3.route("/lab3/")
 def lab():
     return render_template('lab3.html')
 
-
-    @lab3.route("/lab3/")
-def lab():
-    return render_template('lab3.html')
-
-
 @lab3.route("/lab3/forml")
 def forml():
+    errors = {}
     user = request.args.get('user')
-    age = request.args.get('age') 
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+
+    age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'Заполните поле!'
+
     sex = request.args.get('sex')
     return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
