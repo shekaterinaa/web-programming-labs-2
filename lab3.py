bc76a553cdbcab9ebe2bfafc27fb4113ede8b430
Шкуропатова Екатерина <katygetty98@mail.ru>
2023-10-25 17:16:40 +0700

    Форма

--- lab3.py
+++ lab3.py
@@ -1,6 +1,19 @@
-from flask import Blueprint, Flask, redirect, url_for, render_template
+from flask import Blueprint, Flask, render_template, request
 lab3 = Blueprint('lab3',__name__)
 
 @lab3.route("/lab3/")
 def lab():
     return render_template('lab3.html')
+
+
+    @lab3.route("/lab3/")
+def lab():
+    return render_template('lab3.html')
+
+
+@lab3.route("/lab3/forml")
+def forml():
+    user = request.args.get('user')
+    age = request.args.get('age') 
+    sex = request.args.get('sex')
+    return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
