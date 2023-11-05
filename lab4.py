3c9a1aa0899a212a3850c81ae5e5315e5ec99f94
Шкуропатова екатерина <katygetty98@mail.ru>
2023-11-05 19:01:45 +0700

    Начало

--- lab4.py
+++ lab4.py
@@ -4,3 +4,17 @@ lab4 = Blueprint('lab4',__name__)
 @lab4.route("/lab4/")
 def lab():
     return render_template('lab4.html')
+
+@lab4.route("/lab4/login ", methods = ['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == 'alex' and password == '123':
+       return render_template('success.html')
+
+    error = 'Неверные логин и/или пароль'
+    return render_template('login.html', error=error)
+                            
