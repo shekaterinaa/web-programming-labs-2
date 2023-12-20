e6b27ff8ae9d9eb5d3bf7aa896891382b0ac047f
Шкуропатова екатерина <katygetty98@mail.ru>
2023-12-20 14:14:26 +0700

    енерация и редактирвоание

--- lab8.py
+++ lab8.py
@@ -38,3 +38,29 @@ def del_course(course_num):
         return '', 204  # Возвращаем успешный HTTP-статус без содержимого
     else:
         abort(404)
+
+# Маршрут для обновления информации о курсе по индексу
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['PUT'])
+def put_course(course_num):
+    if 0 <= course_num < len(courses):
+        # Получаем данные о курсе из JSON-запроса
+        course = request.get_json()
+        # Обновляем информацию о курсе, используя переданные данные
+        courses[course_num]['name'] = course.get('name', courses[course_num]['name'])
+        courses[course_num]['videos'] = course.get('videos', courses[course_num]['videos'])
+        courses[course_num]['price'] = course.get('price', None)  
+        return jsonify(courses[course_num])
+    else:
+        abort(404)
+
+# Маршрут для добавления нового курса
+@lab8.route('/lab8/api/courses/', methods=['POST'])
+def add_course():
+    # Получаем данные о новом курсе из JSON-запроса
+    course = request.get_json()
+    # Добавляем дополнительные данные, такие как дата создания
+    course['createdAt'] = datetime.now()
+    course['price'] = course.get('price', None)  
+    # Добавляем новый курс в список
+    courses.append(course)
+    return {"num": len(courses)-1}  # Возвращаем номер нового курса в списке
